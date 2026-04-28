3ce6c3672da9322912610860f4405b58c76083df
avant69heim <168681979+avant69heim@users.noreply.github.com>
2026-04-28 15:19:37 -0400

    fix(deploy): static port 8080 for nginx + healthcheck timeout

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,7 +8,7 @@ export const environment = {
 
   // === URL del Backend ===
   apiUrl: 'https://tu-bus-backend-production.up.railway.app/api',
-  clientUrl: 'https://tubusexpress.web.app',
+  clientUrl: 'https://tu-bus-frontend-production.up.railway.app',
 
   appName: 'TuBus Express',
   version: '1.0.0',
