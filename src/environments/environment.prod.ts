29fecb6fef920619faa6bfd89e6cb3131279d6ea
avant69heim <168681979+avant69heim@users.noreply.github.com>
2026-04-30 10:39:06 -0400

    feat(env): point apiUrl to api.tubusexpress.com

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
   production: true,
 
   // === URL del Backend ===
-  apiUrl: 'https://tu-bus-backend-production.up.railway.app/api',
+  apiUrl: 'https://api.tubusexpress.com/api',
 
   appName: 'TuBus Express',
   version: '1.0.0',
